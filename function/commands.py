498387ac5e51c6c1e0002a88065e7b9031aa2d03
Gluttony-sw <farhansw.off@gmail.com>
2023-04-07 10:58:31 +0700

    Updated Fungsi Dasar

--- function/commands.py
+++ function/commands.py
@@ -1,2 +1,27 @@
+# -------------------------------- MODUL BASIC COMMANDS ----------------------------------------
 
+# function separate(sentence : string) -> array
+# { Menerima masukan berupa string dan akan memberikan output pemenggalan mark}
+# { Alternatif fungsi .split() }
 
+# function count(data: array) -> integer
+# { Menerima input array dan memberikan output banyaknya data di array tersebut }
+
+def separate(sentence, mark=" "):
+    split_value = []
+    tmp = ''
+    for c in sentence:
+        if c == mark:
+            split_value.append(tmp)
+            tmp = ''
+        else:
+            tmp += c
+    if tmp:
+        split_value.append(tmp)
+    return split_value
+
+def count(data):
+    counter = 0
+    for i in data:
+        counter += 1
+    return counter
--- function/dataModule.py
+++ function/dataModule.py
@@ -1,4 +1,7 @@
-
+try:
+    import commands
+except:
+    from function import commands
 
 def load(filename, data):
 
@@ -7,43 +10,24 @@ def load(filename, data):
 
     return data
 
-def separate(sentence):
-    split_value = []
-    tmp = ''
-    for c in sentence:
-        if c == ' ':
-            split_value.append(tmp)
-            tmp = ''
-        else:
-            tmp += c
-    if tmp:
-        split_value.append(tmp)
-    return split_value
-
-def count(data):
-    counter = 0
-    for i in data:
-        counter += 1
-    return counter
-
 def cariIndeks(username, data_username):
-    for i in range(count(data_username)):
+    for i in range(commands.count(data_username)):
         if(data_username[i][0] == username):
             return i
     return (-999)
 
 def cariRole(username, data_username):
-    for i in range(count(data_username)):
+    for i in range(commands.count(data_username)):
         if(data_username[i][0] == username):
             return (data_username[i][2])
     return ("Unknown")
 
 def printInfo(username, data_username):
     indeks = cariIndeks(username, data_username)
-    for j in range(count(data_username[indeks])):
+    for j in range(commands.count(data_username[indeks])):
         if (j == 0) : print("Username: ", end="")
         elif(j == 1): print("Password: ", end="")
         elif(j==2)  : print("Role: ", end="")
         print(data_username[indeks][j], end="  ||  ")
 
-        if(j==(count(data_username[indeks])-1)): print("")
+        if(j==(commands.count(data_username[indeks])-1)): print("")
