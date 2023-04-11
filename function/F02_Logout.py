a939a26dd7e55596c3c1b4585a84ee44f78de900
AlthariqFairuz <althariqfairuz273@gmail.com>
2023-04-11 17:29:22 +0700

    FunGSI F14-SAVE (belum kelar)

--- function/F02_Logout.py
+++ function/F02_Logout.py
@@ -1,2 +1,9 @@
-def logout(isLogin):
+# Fungsi Logout                  logout(isLogin)
+def logout(isLogin : bool):
+# Function logout (isLogin : Boolean) -> Boolean akan menerima input berupa Boolean yang bernilai True dan mengembalikan nilai False daru Boolean tersebut
+
+# KAMUS LOKAL 
+# isLogin : Boolean
+
+# ALGORITMA
     return not isLogin
