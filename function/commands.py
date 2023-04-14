df50056b08cfb0a007a706bc804d539d954d8e99
Gluttony-sw <farhansw.off@gmail.com>
2023-04-14 10:05:24 +0700

    Command Fix

--- function/commands.py
+++ function/commands.py
@@ -58,7 +58,7 @@ def test():
     users = [['Bondowoso', 'cintaroro', 'bandung_bondowoso'],['Roro', 'gasukabondo', 'roro_jonggrang'], ['*', '*', '*']]
     print("Count Matriks", countMatriks(users, 3))
 
-test()
-users = [['Bondowoso', 'cintaroro', 'bandung_bondowoso'],['Roro', 'gasukabondo', 'roro_jonggrang'], ['*', '*', '*']]
+# test()
+# users = [['Bondowoso', 'cintaroro', 'bandung_bondowoso'],['Roro', 'gasukabondo', 'roro_jonggrang'], ['*', '*', '*']]
 
-print(excludeEmptyMatriks(users,1,2))
+# print(excludeEmptyMatriks(users,1,2))
