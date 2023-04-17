a7ef1712c7ae5a2c2575307961c483d6858fe7df
AlthariqFairuz <althariqfairuz273@gmail.com>
2023-04-17 20:34:46 +0700

    update F16 + perbaikan beberapa fungsi lainnya

--- function/F02_Logout.py
+++ function/F02_Logout.py
@@ -3,7 +3,7 @@ def logout(isLogin : bool):
 # Function logout (isLogin : Boolean) -> Boolean akan menerima input berupa Boolean yang bernilai True dan mengembalikan nilai False daru Boolean tersebut
 
 # KAMUS LOKAL 
-# isLogin : Boolean
+    isLogin : bool
 
 # ALGORITMA
     return not isLogin
--- function/F12_AyamBerkokok.py
+++ function/F12_AyamBerkokok.py
@@ -1,8 +1,10 @@
-# Fungsi Ayam Berkokok           ayamberkokok(total_candi : int)
+# Fungsi Ayam Berkokok           ayamberkokok(total_candi : int) -> str
 def ayamberkokok (total_candi : int) :
+# I.S fungsi menerima jumlah candi yang sudah terbentuk
+# F.S fungsi mengembalikan hasil akhir dair permainan apakah roro menang atau bondowoso yang menang
     # KAMUS LOKAL :
     
-    # total_candi : int
+    total_candi : int
 
     # ALGORITMA UTAMA
     if total_candi is not None and total_candi < 100 :
