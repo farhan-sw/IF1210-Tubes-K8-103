2229fa7d9b6cde1858e9515a32b6f4a11748edd2
Farhan <farhansw.off@gmail.com>
2023-04-22 09:27:04 +0700

    UP V

--- function/F12_AyamBerkokok.py
+++ function/F12_AyamBerkokok.py
@@ -1,21 +1,19 @@
-# PROSEDUR Ayam Berkokok           ayamberkokok(input total_candi : int) 
+import sys
+sys.path.insert(0, 'function')
+import dataModule
 
-def ayamberkokok (total_candi : int) :
-    
-# I.S fungsi menerima jumlah candi yang sudah terbentuk
-# F.S fungsi mengembalikan hasil akhir dair permainan apakah roro menang atau bondowoso yang menang
-    
+# PROSEDUR AYAM BERKOKOK           ayamberkokok(INPUT candi : list[list[str]], INPUT NMax_candi : int, OUTPUT : String)
+def ayamberkokok (candi : list[list[str]], NMax_candi : int) :
+    # I.S fungsi menerima jumlah candi yang sudah terbentuk
+    # F.S fungsi mengembalikan hasil akhir dair permainan apakah roro menang atau bondowoso yang menang
+
     # KAMUS LOKAL :
-    
+
     total_candi : int
 
     # ALGORITMA UTAMA
+    total_candi = dataModule.hitungCandi(candi, NMax_candi)
+
     if total_candi is not None and total_candi < 100 :
         print("Kukuruyuk.. Kukuruyuk..\n")
         print("Jumlah Candi:",total_candi) 
-        print("\nSelamat, Roro Jonggrang memenangkan permainan!\n\n*Bandung Bondowoso angry noise*\n\nRoro Jonggrang dikutuk menjadi candi.")
-        exit()
-    else : # total_candi >= 100
-        print ("Kukuruyuk.. Kukuruyuk..\n\nJumlah Candi:",str(total_candi)+"\n\nYah, Bandung Bondowoso memenangkan permainan!\n")
-        exit()
-
